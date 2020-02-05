32d049f11e424ba73814d99f31426d27c787e6af
Jordan Lesich <jordan.lesich@gmail.com>
2020-02-05 10:43:42 -0500

    first-big-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
+import WorkSpace from './WorkSpace'
+import {Route, Switch} from 'react-router-dom';
+import FAQ from './FAQ'
+import Options from './Options'
 import './App.css';
+import SideMenu from './SideMenu';
+
 
 function App() {
+  
+ const initialOptions = JSON.parse(window.localStorage.getItem('options')) || {
+    pomTime: 25,
+    breakTime: 5,
+    pomSound: 'timeWave',
+    breakSound: 'chime'
+};
+
+  const [options, setOptions] = useState(initialOptions);
+
+  useEffect(() => {
+    window.localStorage.setItem('options', JSON.stringify(options))
+  },[options]);
+
+
+  const saveOptions = (data) => {
+    const newOptions = {...options, ...data}
+    setOptions(newOptions)
+  }
+
+const sessionDay = 1
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideMenu />
+      <Switch>
+          <Route exact path="/faq">
+            <FAQ />
+          </Route>
+          <Route exact path="/options">
+            <Options saveOptions={saveOptions} options={options}/>
+          </Route>
+          <Route path="/">
+            <WorkSpace sessionDay={sessionDay} options={options}/>
+          </Route>
+      </Switch>
     </div>
   );
 }
